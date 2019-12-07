f68e73c427be0348fb2cafa02aa952f7ffb81d50
Reg Tiangha <reg@reginaldtiangha.com>
2019-12-07 03:07:50 -0700

    Support for Plato v0.7.9 and external sd cards

--- KSM09/adds/kbmenu/onstart/start_plato.sh
+++ KSM09/adds/kbmenu/onstart/start_plato.sh
@@ -1,11 +1,26 @@
 #! /bin/sh
 
+# todo: set rotation value for the different device models
+# currently I know only for sure that trilogy|kraken|alyssum|daylight need 3
+case $PRODUCT in
+  dragon|dahlia|snow ) platoRotate="1";;
+  * ) platoRotate="3";;
+esac
+
+if [ "${platoRotate}" != "$(cat /sys/class/graphics/fb0/rotate)" ]; then
+  echo "${platoRotate}" > /sys/class/graphics/fb0/rotate
+  if [ "${platoRotate}" != "$(cat /sys/class/graphics/fb0/rotate)" ]; then
+    echo "$((platoRotate ^ 2))" > /sys/class/graphics/fb0/rotate
+  fi
+fi
+
 # todo: maybe warn when wifi is on?
 
 #${platobasedir}/plato.sh
 cd "${platobasedir}" || exit 1
 export LD_LIBRARY_PATH="libs:${LD_LIBRARY_PATH}"
 export MODEL_NUMBER=$(cut -f 6 -d ',' /mnt/onboard/.kobo/version | sed -e 's/^[0-]*//')
+grep -q ' /mnt/sd .*[ ,]ro[ ,]' /proc/mounts && mount -o remount,rw /mnt/sd
 ./plato > info.log 2>&1 || mv info.log crash.log
 
 # todo: maybe warn when wifi is on?
