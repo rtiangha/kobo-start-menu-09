822bb2a8d8d09be4209b3f5a86e684fda0574b8a
Reg Tiangha <reg@reginaldtiangha.com>
2019-12-12 19:54:04 -0700

    Fix script

--- KSM09/adds/kbmenu/onstart/start_plato.sh
+++ KSM09/adds/kbmenu/onstart/start_plato.sh
@@ -6,7 +6,7 @@
 cd "${platobasedir}" || exit 1
 export LD_LIBRARY_PATH="libs:${LD_LIBRARY_PATH}"
 export MODEL_NUMBER=$(cut -f 6 -d ',' /mnt/onboard/.kobo/version | sed -e 's/^[0-]*//')
-grep -q ' /mnt/sd .*[ ,]ro[ ,]' /proc/mounts && mount -o remount,rw /mnt/sd
+mount -o remount,rw /mnt/sd
 ./plato > info.log 2>&1 || mv info.log crash.log
 
 # todo: maybe warn when wifi is on?
