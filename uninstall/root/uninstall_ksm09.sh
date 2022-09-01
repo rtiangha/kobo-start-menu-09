122fca6aceebc461359e81e5d5ad1ddfc12eee87
Reg Tiangha <reg@reginaldtiangha.com>
2022-08-31 18:55:31 -0600

    Create uninstaller for firmware 4.33.19759

---
+++ uninstall/root/uninstall_ksm09.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf /adds
+rm -rf /isforksm.txt
+rm -rf /mnt/onboard/.adds/kbmenu_user
+rm -rf /mnt/onboard/.adds/wget
+
+rm /etc/udev/rules.d/uninstall_ksm09.rules
+rm /root/uninstall_ksm09.sh 
